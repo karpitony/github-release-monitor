89bb3ab8aaa150d6794ffbc585b94032acaf2acf
YEAHx4 <developerstarlight@gmail.com>
2024-06-30 23:42:57 +0900

    Dashboard

---
+++ js/index.js
@@ -0,0 +1,100 @@
+async function fetchConfig() {
+  try {
+    const res = await fetch('/config.json')
+    return res.json()
+  } catch (err) {
+    console.err('Failed to fetch config file')
+    console.err(err)
+  }
+}
+
+let popup = null
+let content = null
+let totalCanvas = null
+let popupTitle = null
+
+function renderFolderList(folders) {
+  const container = document.getElementById('folder-container')
+  folders.forEach(name => {
+    const child = document.createElement('div')
+    child.classList.add('folder-name')
+    child.innerText = name
+
+    const drawTotalChart = (csv) => {
+      const header = csv[0];
+      const body = csv.slice(1).reverse()
+      console.log(header)
+      console.log(body)
+
+      const labels = body.map(line => line[0])
+      console.log(labels)
+      const ctx = totalCanvas.getContext('2d')
+
+      const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: 'Total Downloads',
+            data: body.map(line => line[1]),
+            borderColor: '#000',
+            borderWidth: 1,
+            fill: false
+          }]
+        }
+      })
+
+      console.log(chart)
+    }
+
+    const prepare = async () => {
+      res = await fetch(`/${name}/${name}_total.csv`)
+      const totalCsv = parseCsv(await res.text())
+    
+      popupTitle.innerText = name
+      drawTotalChart(totalCsv)
+      showPopup()    
+    }
+
+    child.onclick = prepare
+    container.appendChild(child)
+  })
+}
+
+function parseCsv(content) {
+  return content
+    .split('\n')
+    .filter(it => it)
+    .map(line => line.split(',').map(it => it.trim()))
+}
+
+
+function showPopup() {
+  popup.classList.remove('hidden')
+}
+
+function hidePopup() {
+  popup.classList.add('hidden')
+}
+
+async function start() {
+  const config = await fetchConfig()
+  const folderList = config.folder_list
+  console.dir(folderList)
+
+  popup = document.getElementById('popup-bg')
+  content = document.getElementById('popup-content')
+  totalCanvas = document.getElementById('total-canvas')
+  popupTitle = document.getElementById('popup-title')
+
+  document.getElementById('popup-close').onclick = hidePopup
+  document.getElementById('popup-bg').onclick = hidePopup
+  document.getElementById('popup').onclick = e => {
+    e.stopPropagation()
+  }
+  hidePopup()
+
+  renderFolderList(folderList)
+}
+
+document.addEventListener('DOMContentLoaded', start)
